542fb24c4b2c7ed40ac05f4e3e1916af8a85f523
Karthikraut <karthikraut2@gmail.com>
2024-06-13 17:56:47 +0530

    Model for tweet is created and tweete is created.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express  =require("express");
 const connect  = require("./config/databases")
 const app = express();
+const Tweet = require('./models/tweet');
 
 app.listen(3000,async ()=>{
     console.log("Server Started");
     await connect();
+    const tweet  =await Tweet.create({
+        content: "First Tweeet",
+        email:  "a&b@gmail.com"
+    })
     console.log("Mongoose Connected");
+    console.log(tweet);
 })
